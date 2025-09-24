3d90aef4d74598f40010f57e80a8b42e2d30bb68
raonegone-max <aryangupta.in@gmail.com>
2025-09-24 21:47:38 +0530

    comm

---
+++ ronak/tic-tac-toe/script.js
@@ -0,0 +1,81 @@
+let count = 0;
+let boxes = document.querySelectorAll('.box');
+let reset = document.querySelector('#reset');
+let turnO = true; // player X , player O
+
+let resetBtn = document.querySelector('#reset');
+let msgContainer = document.querySelector('.msg-container');
+let msg = document.querySelector('#msg');
+
+const winPatterns = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
+];
+const resetGame = () => {
+  turnO = true;
+  count = 0;
+  enableBoxes();
+  msgContainer.classList.add('hide');
+};
+boxes.forEach((box) => {
+  box.addEventListener('click', () => {
+    console.log('box was clicked');
+    if (turnO) {
+      // Player O
+      box.innerText = 'O';
+      turnO = false;
+    } else {
+      // Player X
+      box.innerText = 'X';
+      turnO = true;
+    }
+    box.disabled = true;
+    count++;
+    checkWinner();
+  });
+});
+
+const disableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = true;
+  }
+};
+
+const enableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = false;
+    box.innerText = '';
+  }
+};
+
+const showWinner = (Winner) => {
+  msg.innerText = `Congratulations , Winner is ${Winner}`;
+  msgContainer.classList.remove('hide');
+  disableBoxes();
+};
+
+const checkWinner = () => {
+  for (let pattern of winPatterns) {
+    let pos1Val = boxes[pattern[0]].innerText;
+    let pos2Val = boxes[pattern[1]].innerText;
+    let pos3Val = boxes[pattern[2]].innerText;
+
+    if (pos1Val != '' && pos2Val != '' && pos3Val != '') {
+      if (pos1Val === pos2Val && pos2Val == pos3Val) {
+        console.log('Winner', pos1Val);
+        showWinner(pos1Val);
+      }
+    }
+  }
+  if (count === 9) {
+    msg.innerText = 'The Match is Draw ';
+    msgContainer.classList.remove('hide');
+  }
+};
+resetBtn.addEventListener('click', resetGame);
